ff980812963424d9ddbfa9241ce7cda811374590
Erickbut <erickbutron1@gmail.com>
2022-12-09 22:56:40 -0400

    main interface

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,10 @@
+//? Previamente debe pasar por el middleware
+const roleMiddleware = (req, res, next)=>{
+if(req.user.role=== 'admin'){
+  next()
+}else{
+  res.status(401).json({message:'Permission Denied'})
+}
+}
+
+module.exports = roleMiddleware
